917de4f62affa3617871c4fb4ea1a04dcd62e4cf
Isha Khanal <ikhanal@sevadev.com>
2020-10-20 10:28:48 +0545

    heroku deploy

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
@@ -132,3 +132,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
